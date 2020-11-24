9022c9545e565782ceb9747d999efb3131c50c47
Tremayne Terry <tremaynet@gmail.com>
2020-11-24 00:12:29 -0500

    update project structure to run in heroku

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,2 @@
 Use burgers_db;
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false)
-INSERT INTO burgers (burger_name, devoured) VALUES ('Jr Bacon Cheeseburger', false)
-INSERT INTO burgers (burger_name, devoured) VALUES ('Jr Bacon burger', false)
